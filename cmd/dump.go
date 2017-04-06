d3b8222e01f9f098773857b359bcc81bfa31f5f8
David <da70@nyu.edu>
2017-04-06 17:53:15 -0400

    cmd/: remove cobra boilerplate comments

--- cmd/dump.go
+++ cmd/dump.go
@@ -1,17 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -21,7 +7,6 @@ import (
 	"github.com/nyulibraries/enm/tct"
 )
 
-// dumpCmd represents the dump command
 var dumpCmd = &cobra.Command{
 	Use:   "dump",
 	Short: "Do a print dump of all retrieved JSON data",
@@ -64,15 +49,4 @@ var dumpCmd = &cobra.Command{
 
 func init() {
 	devCmd.AddCommand(dumpCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// dumpCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// dumpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }

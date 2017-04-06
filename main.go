00620565e4ccf43e2a84934a25031b6839e3c7ca
David <da70@nyu.edu>
2017-04-05 21:08:54 -0400

    main.go: test GetNamesAll()

--- main.go
+++ main.go
@@ -12,4 +12,10 @@ func main() {
 
 	topicDetail := GetTopicDetail(7399)
 	fmt.Println(topicDetail)
+
+	namesList := GetNamesAll()
+	for i, v := range namesList {
+		fmt.Printf("Name #%d: %v\n", i, v)
+	}
+
 }

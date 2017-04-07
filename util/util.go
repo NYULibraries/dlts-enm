d42762b26c04a87991ab567017b0dbf6d6615728
David <da70@nyu.edu>
2017-04-07 16:33:08 -0400

    util.go: remove RemoveContents()

--- util/util.go
+++ util/util.go
@@ -4,24 +4,3 @@ import(
 	"path/filepath"
 	"os"
 )
-
-// Source: http://stackoverflow.com/questions/33450980/golang-remove-all-contents-of-a-directory
-// Author: http://stackoverflow.com/users/221700/peterso
-func RemoveContents(dir string) error {
-	d, err := os.Open(dir)
-	if err != nil {
-		return err
-	}
-	defer d.Close()
-	names, err := d.Readdirnames(-1)
-	if err != nil {
-		return err
-	}
-	for _, name := range names {
-		err = os.RemoveAll(filepath.Join(dir, name))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}

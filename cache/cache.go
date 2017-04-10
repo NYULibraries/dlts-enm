9851accb39572b024294326167ed327079cfc4d1
David <da70@nyu.edu>
2017-04-10 12:44:15 -0400

    cache: init by creating (updated) cache directory if it does not already exist

--- cache/cache.go
+++ cache/cache.go
@@ -1,11 +1,23 @@
 package cache
 
+import (
+	"os"
+)
+
 // TODO:
 //   * Figure out if there's a better place to put this
 //   * Allow user to specify cache path
 // Tried using os.TempDir(), but it was returning
 // /var/folders/dh/48wd7vnj3xqd1w_f126tcnvh0000gn/T/, which was not as convenient.
-var Cache = "/tmp"
+var Cache = "/tmp/enm-cache"
+
+func init() {
+	if _, err := os.Stat(Cache); os.IsNotExist(err) {
+		os.Mkdir(Cache, 0700)
+	} else if err != nil {
+		panic(err.Error())
+	}
+}
 
 func CacheFile(request string, id string) (cacheFile string) {
 	cacheFile = Cache + "/" + request;

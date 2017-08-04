93e31839d64c9f942be5ee92ec10621142cb0ed4
David <da70@nyu.edu>
2017-08-04 12:58:41 -0400

    solr/solrutil.go: initial commit

---
+++ solr/solrutil.go
@@ -0,0 +1,27 @@
+package solrutil
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/nyulibraries/dlts-enm/db/models"
+	"github.com/rtt/Go-Solr"
+)
+
+var Port int
+var Server string
+
+func Init(server string, port int) error {
+	s, err := solr.Init(server, port, "enm-topics")
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("solr.Init(): %v\n", s)
+
+	return nil
+}
+
+func AddPage(page *models.Page) error {
+	return errors.New("Error!")
+}

0b13271053444b21a69b3f31db5f8e1223642aef
David <da70@nyu.edu>
2017-12-21 12:53:06 -0500

    sitegen.GenerateTopicPage BUGFIX: cast int64 to int

--- sitegen/sitegen.go
+++ sitegen/sitegen.go
@@ -117,7 +117,7 @@ func GenerateTopicPage(topicID int, topicDisplayName string, alternateNames []st
 			Title: epubMatchForTopicWithNumberOfMatchedPages.Title,
 			Authors: epubMatchForTopicWithNumberOfMatchedPages.Author,
 			Publisher: epubMatchForTopicWithNumberOfMatchedPages.Publisher,
-			NumberOfOccurrences: epubMatchForTopicWithNumberOfMatchedPages.NumberOfOccurrences,
+			NumberOfOccurrences: int(epubMatchForTopicWithNumberOfMatchedPages.NumberOfOccurrences),
 		})
 	}
 

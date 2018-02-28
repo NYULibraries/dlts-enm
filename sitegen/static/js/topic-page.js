792d352a0f4ef099ef10a12cac62bc4776ae39cc
David <da70@nyu.edu>
2018-02-28 15:04:42 -0500

    sitegen/static/js/topic-page.js: whitespace reformatting and some re-arranging of call

--- sitegen/static/js/topic-page.js
+++ sitegen/static/js/topic-page.js
@@ -11,54 +11,51 @@ var basepath               = window.location.href.split( '/' ).slice( 0, -5 ).jo
     // visualizationData is defined and initialized in a previous <script> tag
     forceSimulation = d3.forceSimulation().nodes( visualizationData.nodes ),
     linkForce   = d3.forceLink( visualizationData.links )
-                        .id(
-                            function ( d ) {
-                                return d.id;
-                            }
-                        ),
+        .id(
+            function ( d ) {
+                return d.id;
+            }
+        ),
     chargeForce = d3.forceManyBody()
-                        .strength( -3500 )
-                        .distanceMax( 500 )
-                        .distanceMin( 100 ),
+        .strength( -3500 )
+        .distanceMax( 500 )
+        .distanceMin( 100 ),
 
-    wrapperG    = svg.append( "g" ).attr( "class", "holdAll" ),
+    wrapperG = svg.append( "g" ).attr( "class", "holdAll" ),
 
-    link        = wrapperG.append( "g" )
-        .attr( "class", "links" )
+    link = wrapperG.append( "g" )
+            .attr( "class", "links" )
         .selectAll( "line" )
         .data( visualizationData.links )
         .enter().append( "line" )
-        .attr( "stroke-width", .7 )
-        .style( "stroke", "black" ),
+            .attr( "stroke-width", .7 )
+            .style( "stroke", "black" ),
 
-    node        = wrapperG.append( "g" )
-        .attr( "class", "nodes" )
+    node = wrapperG.append( "g" )
+            .attr( "class", "nodes" )
         .selectAll( "circle" )
         .data( visualizationData.nodes )
         .enter().append( "g" )
-        .attr( "class", "node" )
-        .attr( "id", getId )
-        .attr( "data-ocount", function ( d ) {
-            return d.ocount;
-        } )
-        .attr( "data-nodenum", function ( d ) {
-            return d.id;
-        } )
-        .attr( "class", isActive )
-        .call( d3.drag()
-                   .on( "start", dragstarted )
-                   .on( "drag", dragged )
-                   .on( "end", dragended ) ),
+            .attr( "class", "node" )
+            .attr( "id", getId )
+            .attr( "data-ocount", function ( d ) {
+                return d.ocount;
+            } )
+            .attr( "data-nodenum", function ( d ) {
+                return d.id;
+            } )
+            .attr( "class", isActive )
+            .call( d3.drag()
+                .on( "start", dragstarted )
+                .on( "drag", dragged )
+                .on( "end", dragended ) ),
 
     zoomHandler = d3.zoom().on( "zoom", zoom_actions );
-
-forceSimulation.force( "charge_force", chargeForce )
-    .force( "center_force", d3.forceCenter( width / 4, height / 3 ) )
-    .force( "links", linkForce );
 
 node.append( "circle" )
     .attr( "r", calculateRadius )
     .attr( "class", "circle" );
+
 node.append( "text" )
     .attr( "dx", determineOffset )
     .attr( "dy", ".35em" )
@@ -66,9 +63,14 @@ node.append( "text" )
         return d.name;
     } )
     .attr( "font-size", "1rem" );
+
 node.on( "click", function ( d ) {
     window.location.href = basepath + '/' + d.path;
 } );
+
+forceSimulation.force( "charge_force", chargeForce )
+    .force( "center_force", d3.forceCenter( width / 4, height / 3 ) )
+    .force( "links", linkForce );
 
 zoomHandler( svg );
 

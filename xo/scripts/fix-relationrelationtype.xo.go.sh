4721d1f915b361c9a577e218cefa8d4e90074b02
David <da70@nyu.edu>
2018-04-06 19:13:56 -0400

    xo/scripts/fix-relationrelationtype.xo.go.sh: now works for both BSD and GNU sed (Mac OS X and Linux)

--- xo/scripts/fix-relationrelationtype.xo.go.sh
+++ xo/scripts/fix-relationrelationtype.xo.go.sh
@@ -5,5 +5,10 @@
 # Sed command from https://stackoverflow.com/questions/45147094/sed-replace-multiline
 # Added "// Duplicate Rtype field declaration removed" replacement text because
 # otherwise an empty line is left in the file, which looks weird.
-sed -i '' '/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/{n;s/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/\/\/ Duplicate Rtype field declaration removed/;}' \
+# Note that there doesn't seem to be a way to inline sed without backup file
+# that works on both Mac OS X (BSD sed) and Linux (GNU sed), so just make a
+# backup file and delete it.
+sed -i.bak '/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/{n;s/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/\/\/ Duplicate Rtype field declaration removed/;}' \
     db/postgres/models/relationrelationtype.xo.go
+
+rm db/postgres/models/relationrelationtype.xo.go.bak

2b0fe5660eb8cac488b0be7776fcd370b4f1c273
David <da70@nyu.edu>
2018-06-05 18:04:12 -0400

    sitegen/sitegen.go: change template directory constants to vars so they can be customized by topicpages_test.go

--- sitegen/sitegen.go
+++ sitegen/sitegen.go
@@ -22,11 +22,16 @@ var Source string
 
 // Tricky...this assumes that location of the templates relative to working directory
 // matches what the location would be if calling `go run main.go` from root of this
-// repo.  This is brittle: user could call `go run ../main.go` from sitegen/, or
-// could be calling the built binary from anywhere relative to templates.
+// repo.  This is brittle, breaking in multiple cases:
+//
+// * Doing `go run [PATH TO main.go` or calling the binary from somewhere relative
+//   to the templates directory other than what's assumed below.
+// * `go test` on tests in a directory that would set working directory to some
+//   place other than what's assumed below.
+//
 // Consider including the templates in the binary using something like
 // https://github.com/jteeuwen/go-bindata
-const BrowseTopicListsTemplateDirectory = "sitegen/templates/browse-topics-lists"
-const SharedTemplateDirectory = "sitegen/templates/shared"
-const SitePagesTemplateDirectory = "sitegen/templates"
-const TopicPageTemplateDirectory =  "sitegen/templates/topic-page"
+var BrowseTopicListsTemplateDirectory = "sitegen/templates/browse-topics-lists"
+var SharedTemplateDirectory = "sitegen/templates/shared"
+var SitePagesTemplateDirectory = "sitegen/templates"
+var TopicPageTemplateDirectory =  "sitegen/templates/topic-page"

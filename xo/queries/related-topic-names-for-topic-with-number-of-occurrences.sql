bd4354098374f2dea6068ae51000592f1f918177
David <da70@nyu.edu>
2018-06-05 13:54:34 -0400

    xo/queries/related-topic-names-for-topic-with-number-of-occurrences.sql{.README}: update for informational purposes only and add README for explanation

--- xo/queries/related-topic-names-for-topic-with-number-of-occurrences.sql
+++ xo/queries/related-topic-names-for-topic-with-number-of-occurrences.sql
@@ -1,10 +1,16 @@
 SELECT trs.topic2_id, t.display_name_do_not_use, COUNT( * ) AS number_of_occurrences
-
 FROM topic_relations_simple trs INNER JOIN topics t ON trs.topic2_id = t.tct_id
-  INNER JOIN occurrences o ON o.topic_id = trs.topic2_id
-
-WHERE topic1_id = %%topic_id int%%
-
+INNER JOIN occurrences o ON o.topic_id = trs.topic2_id
+WHERE trs.topic1_id = %%topic_id int%%
 GROUP BY trs.topic2_id, t.display_name_do_not_use
 
-ORDER BY t.display_name_do_not_use
+UNION
+
+SELECT trs.topic2_id, t.display_name_do_not_use, 0 AS number_of_occurrences
+FROM topic_relations_simple trs INNER JOIN topics t ON trs.topic2_id = t.tct_id
+LEFT OUTER JOIN occurrences o ON o.topic_id = trs.topic2_id
+WHERE trs.topic1_id = %%topic_id int%%
+AND o.topic_id IS NULL
+GROUP BY trs.topic2_id, t.display_name_do_not_use
+
+ORDER BY display_name_do_not_use

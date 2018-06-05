e7e58c2e91e005c8b763900046b638c7bbd2471a
David <da70@nyu.edu>
2018-06-05 13:50:41 -0400

    xo/scripts/generate-models-for-topic-page-queries.sh: disable model generation based on related-topic-names-for-topic-with-number-of-occurrences.sql and add comment explaining why

--- xo/scripts/generate-models-for-topic-page-queries.sh
+++ xo/scripts/generate-models-for-topic-page-queries.sh
@@ -10,12 +10,19 @@ function xo_generate() {
         --query-strip \
         --query "${query}" \
         --query-type $queryType \
-        --out db/models/
+        --out db/mysql/models/
 
 }
 
 xo_generate xo/queries/topic-number-of-occurrences.sql TopicNumberOfOccurrences
 xo_generate xo/queries/topics-alternate-names.sql TopicAlternateName
 xo_generate xo/queries/epubs-for-topic-with-number-of-matched-pages.sql EpubsForTopicWithNumberOfMatchedPages
-xo_generate xo/queries/related-topic-names-for-topic-with-number-of-occurrences.sql RelatedTopicNamesForTopicWithNumberOfOccurrences
+# NOTE: This model was originally created by this script, but in order to fix a
+# bug which caused zero-occurrence topics to be omitted, the SQL file had to be
+# updated to UNION the omitted topics.
+# Due to a bug in mysql versions below 8.0.0, `xo` is unable to generate mysql
+# models based on UNION queries.
+# For details, see:
+#     https://jira.nyu.edu/jira/browse/NYUP-414?focusedCommentId=99202&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-99202
+#xo_generate xo/queries/related-topic-names-for-topic-with-number-of-occurrences.sql RelatedTopicNamesForTopicWithNumberOfOccurrences
 xo_generate xo/queries/external-relations-for-topic.sql ExternalRelationsForTopic

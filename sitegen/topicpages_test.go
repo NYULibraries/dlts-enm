e5317f048be2c6590502191a8a86a8440eb72900
David <da70@nyu.edu>
2018-06-05 18:06:21 -0400

    sitegen/topicpages_test.go: create topic page files to test

--- sitegen/topicpages_test.go
+++ sitegen/topicpages_test.go
@@ -4,19 +4,55 @@ import (
 	"path/filepath"
 	"testing"
 	"strings"
+	"os"
+	"path"
+
+	"github.com/nyulibraries/dlts-enm/util"
 )
 
-const TopicPagesGoldenFilesDirectory = "testdata/topicpages"
+const TopicPagesGoldenFilesDirectory = "testdata/golden/topic-pages"
 
 func TestGenerateTopicPages(t *testing.T) {
+	wd, err := os.Getwd()
+	if (err != nil) {
+		t.Fatal( "os.Getwd() failed: " + err.Error())
+	}
+
+	rootDirectory := path.Dir(wd)
+
+	destination := rootDirectory + "/sitegen/testdata/tmp"
+	err = os.RemoveAll(destination)
+	if (err != nil) {
+		t.Fatal( "os.Remove(" + destination + ") failed: " + err.Error())
+	}
+
 	Source = "cache"
-	goldenFiles, err := filepath.Glob(TopicPagesGoldenFilesDirectory + "/*.golden")
+	BrowseTopicListsTemplateDirectory = rootDirectory + "/" + BrowseTopicListsTemplateDirectory
+	SharedTemplateDirectory = rootDirectory + "/" + SharedTemplateDirectory
+	SitePagesTemplateDirectory = rootDirectory + "/" + SitePagesTemplateDirectory
+	TopicPageTemplateDirectory = rootDirectory + "/" + TopicPageTemplateDirectory
+
+	Source = "database"
+
+	goldenFiles := []string{}
+	err = filepath.Walk(TopicPagesGoldenFilesDirectory, func(path string, f os.FileInfo, err error) error {
+		if (f.IsDir()) {
+			return nil
+		} else if (strings.HasSuffix(path, ".html")) {
+			goldenFiles = append(goldenFiles, path)
+		} else {
+			t.Fatal("Unexpected file encountered: " + path)
+		}
+
+		return nil
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	for _, goldenFile := range goldenFiles {
-		pieces := strings.SplitN(goldenFile, ".", 2)
-		TopicIDs = append(TopicIDs, strings.TrimLeft(pieces[0],TopicPagesGoldenFilesDirectory + "/"))
+		TopicIDs = append(TopicIDs, util.GetTopicIDFromTopicPagePath(goldenFile))
 	}
+
+	GenerateTopicPages(destination)
 }

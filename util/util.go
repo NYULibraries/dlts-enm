a74ee0f18ae205e48df72ce02be608a59c17ba41
David <da70@nyu.edu>
2018-06-11 17:03:05 -0400

    util.Diff: initial commit

--- util/util.go
+++ util/util.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"strings"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"path"
 )
@@ -33,6 +34,24 @@ func CreateFileWithAllParentDirectories(file string) (f *os.File, err error) {
 	}
 
 	return
+}
+
+func Diff(path1 string, path2 string) (string, error) {
+	diffCmd := "diff"
+
+	outputBytes, err := exec.Command(diffCmd, "-u", "-r", path1, path2).CombinedOutput()
+	if err != nil {
+		switch err.(type) {
+			case *exec.ExitError:
+				// `diff` ran successfully with non-zero exit code.  Report the
+				// differences.
+			default:
+				// `diff` command failed to run.
+				return "", err
+		}
+	}
+
+	return string(outputBytes), nil
 }
 
 func GetMapKeys(m map[string]string) (keys []string) {

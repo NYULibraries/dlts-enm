4051e41ef6b88d5e8079320459c61446eab66dc3
David <da70@nyu.edu>
2018-06-11 17:14:10 -0400

    util.Diff: remove -u from diff call

--- util/util.go
+++ util/util.go
@@ -39,7 +39,7 @@ func CreateFileWithAllParentDirectories(file string) (f *os.File, err error) {
 func Diff(path1 string, path2 string) (string, error) {
 	diffCmd := "diff"
 
-	outputBytes, err := exec.Command(diffCmd, "-u", "-r", path1, path2).CombinedOutput()
+	outputBytes, err := exec.Command(diffCmd, "-r", path1, path2).CombinedOutput()
 	if err != nil {
 		switch err.(type) {
 			case *exec.ExitError:

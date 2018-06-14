af8d5b7ac5d21449f94ad8f880b52c9d1a379bf7
David <da70@nyu.edu>
2018-06-14 11:23:41 -0400

    sitegen/browsetopicslists_test.go: initial commit

---
+++ sitegen/browsetopicslists_test.go
@@ -0,0 +1,47 @@
+package sitegen
+
+import (
+	"testing"
+	"os"
+	"path"
+
+	"github.com/nyulibraries/dlts-enm/util"
+)
+
+func TestGenerateBrowseTopicsLists(t *testing.T) {
+	wd, err := os.Getwd()
+	if (err != nil) {
+		t.Fatal( "os.Getwd() failed: " + err.Error())
+	}
+
+	rootDirectory := path.Dir(wd)
+
+	BrowseTopicsListsGoldenFilesDirectory := rootDirectory + "/sitegen/testdata/golden/browse-topics-lists"
+
+	destination := rootDirectory + "/sitegen/testdata/tmp"
+	outputDir := destination + "/browse-topics-lists"
+	err = os.RemoveAll(outputDir)
+	if (err != nil) {
+		t.Fatal( "os.Remove(" + destination + ") failed: " + err.Error())
+	}
+
+	Source = "cache"
+	BrowseTopicListsTemplateDirectory = rootDirectory + "/" + BrowseTopicListsTemplateDirectory
+	SharedTemplateDirectory = rootDirectory + "/" + SharedTemplateDirectory
+	SitePagesTemplateDirectory = rootDirectory + "/" + SitePagesTemplateDirectory
+	TopicPageTemplateDirectory = rootDirectory + "/" + TopicPageTemplateDirectory
+
+	Source = "database"
+
+	GenerateBrowseTopicsLists(destination)
+
+	diffOutput, err := util.Diff(BrowseTopicsListsGoldenFilesDirectory, outputDir)
+	if (err != nil) {
+		t.Fatal("Diff of " + BrowseTopicsListsGoldenFilesDirectory + " and " +
+			destination + " failed to run: " + err.Error())
+	}
+
+	if (diffOutput != "") {
+		t.Errorf("%s", diffOutput)
+	}
+}

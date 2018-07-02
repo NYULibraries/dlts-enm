0ea77968448fe9a13bd6bc64742bd710047abc53
David <da70@nyu.edu>
2018-07-02 17:01:00 -0400

    Separate xo/scripts/* into mysql/ and postgres/ subdirectories

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 )
 
 //go:generate xo postgres://${ENM_POSTGRES_DATABASE_USERNAME}:${ENM_POSTGRES_DATABASE_PASSWORD}@localhost/${ENM_POSTGRES_DATABASE}?sslmode=disable -o db/postgres/models/ --template-path $PWD/xo/templates/
-//go:generate xo/scripts/fix-relationrelationtype.xo.go.sh
+//go:generate xo/scripts/postgres/fix-relationrelationtype.xo.go.sh
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {

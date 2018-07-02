8caad13533b6473639be18837ae40f3ab071165a
David <da70@nyu.edu>
2018-07-02 18:47:29 -0400

    BUGFIX xo/scripts/postgres/fix-relationrelationtype.xo.go.sh: remove all other rtype duplicate errors
    See https://jira.nyu.edu/jira/browse/NYUP-397?focusedCommentId=100337&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-100337

--- xo/scripts/postgres/fix-relationrelationtype.xo.go.sh
+++ xo/scripts/postgres/fix-relationrelationtype.xo.go.sh
@@ -11,4 +11,21 @@
 sed -i.bak '/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/{n;s/^[[:space:]]*Rtype       string         `json:"rtype"`       \/\/ rtype/\/\/ Duplicate Rtype field declaration removed/;}' \
     db/postgres/models/relationrelationtype.xo.go
 
+sed -i.bak 's/EXCLUDED.rtype, EXCLUDED.rtype,/EXCLUDED.rtype,/g' \
+    db/postgres/models/relationrelationtype.xo.go
+sed -i.bak 's/&rr.Rtype, &rr.Rtype,/\&rr.Rtype,/g' db/postgres/models/relationrelationtype.xo.go
+sed -i.bak 's/rr.Rtype, rr.Rtype,/rr.Rtype,/g'    db/postgres/models/relationrelationtype.xo.go
+sed -i.bak 's/rtype, rtype,/rtype,/g'             db/postgres/models/relationrelationtype.xo.go
+
+# These could probably be safer...originally started with multiline matching
+# but it became unwieldy.  These probably will break if the number of columns
+# changes.  In the end might completely do away with this model, or else might
+# fork knq/xo and bugfix it.
+sed -i.bak 's/`$1, $2, $3, $4, $5, $6` +/`$1, $2, $3, $4, $5` +/g' \
+    db/postgres/models/relationrelationtype.xo.go
+sed -i.bak 's/`$1, $2, $3, $4, $5, $6, $7` +/`$1, $2, $3, $4, $5, $6` +/g' \
+    db/postgres/models/relationrelationtype.xo.go
+sed -i.bak 's/`) WHERE id = $7`/`) WHERE id = $6`/g' \
+    db/postgres/models/relationrelationtype.xo.go
+
 rm db/postgres/models/relationrelationtype.xo.go.bak

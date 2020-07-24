38e456bb4bde068704b43ca6c5233b01d3bceb6c
David <da70@nyu.edu>
2020-07-24 16:50:38 -0400

    cache/cache.go: add caching functions for `sitegen browsetopicslists`

--- cache/cache.go
+++ cache/cache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"github.com/nyulibraries/dlts-enm/util"
 	"os"
+	"path"
 	"strconv"
 )
 
@@ -15,12 +16,18 @@ import (
 //       * reload command
 //       * sitegentopicpages command
 const cache = "/tmp/enm-cache"
+const SitegenBrowseTopicListsCache = cache + "/sitegen-browsetopiclists"
 const SitegenTopicpagesCache = cache + "/sitegen-topicpages"
 const SolrLoadCache = cache + "/solr-load"
 
 func init() {
 	if _, err := os.Stat(cache); os.IsNotExist(err) {
-		err := os.MkdirAll(SitegenTopicpagesCache, 0700)
+		err := os.MkdirAll(SitegenBrowseTopicListsCache, 0700)
+		if (err != nil) {
+			panic(err)
+		}
+
+		err = os.MkdirAll(SitegenTopicpagesCache, 0700)
 		if (err != nil) {
 			panic(err)
 		}
@@ -47,6 +54,18 @@ func CacheFile(request string, id string) (cacheFile string) {
 
 func SolrLoadCacheFile(isbn string, page string, id int) (cacheFile string) {
 	cacheFile = SolrLoadCache + "/" + isbn + "/" + page + "__" + strconv.Itoa(id) + ".json"
+
+	return
+}
+
+func SitegenBrowseTopicListsCategoriesCacheFile() (cacheFile string) {
+	cacheFile = path.Join(SitegenBrowseTopicListsCache, "categories.json")
+
+	return
+}
+
+func SitegenBrowseTopicListsCategoryCacheFile(fileBasename string) (cacheFile string) {
+	cacheFile = path.Join(SitegenBrowseTopicListsCache, fileBasename + ".json")
 
 	return
 }
